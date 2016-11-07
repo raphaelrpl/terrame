3a84c7cf6780f652e13404d45a32189ab17bcbc9
raphaelrpl <raphael.wcosta@gmail.com>
2016-11-07 09:43:14 -0200

    adding ci unittest unix-like environment

---
+++ build/scripts/linux/terrame-ci-unittest.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -1
+# 
+## It enables to run terrame unittest on UNIX environments.
+## **Remember to export some variables before**
+# 
+## TERRAME_PATH - Path to TerraME installation
+## TERRAME_GIT_DIR - Path to TerraME git directory
+#
+## Usage
+# ./terrame-ci-unittest.sh PACKAGE COMMIT
+# 
+# PACKAGE - Represents which package will be tested
+# COMMIT - Pull Request Commit Hash used to notify GitHub
+
+# GitHub Status Name
+GITHUB_CONTEXT="Functional test of package base"
+# GitHub Commit hash
+COMMIT=$2
+# GitHub Status URL
+TARGET_URL="$BUILD_URL/consoleFull"
+
+# Exporting TerraME environment variables
+export TME_PATH="$TERRAME_PATH/bin"
+export PATH=$PATH:$TME_PATH
+export LD_LIBRARY_PATH=$TME_PATH
+
+# Sending initial status (pending) to GitHub
+$TERRAME_GIT_DIR/build/scripts/linux/terrame-ci-notify.sh $COMMIT "$GITHUB_CONTEXT" "pending" "$TARGET_URL" "Running tests."
+
+terrame -version
+terrame -color -test test.lua 2> /dev/null
+# Retrieving TerraME exit code
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+  GITHUB_STATUS="success"
+  GITHUB_DESCRIPTION="Tests executed successfully"
+elif [ $RESULT -eq 255 ]; then
+  GITHUB_STATUS="failure"
+  GITHUB_DESCRIPTION="$RESULT or more errors found"
+else
+  GITHUB_STATUS="failure"
+  GITHUB_DESCRIPTION="$RESULT tests failed"
+fi
+# Removing log files
+rm -rf .terrame*
+
+$TERRAME_GIT_DIR/build/scripts/linux/terrame-ci-notify.sh $COMMIT "$GITHUB_CONTEXT" "$GITHUB_STATUS" "$TARGET_URL" "$GITHUB_DESCRIPTION"
+
+exit $RESULT
