717ab2cfb13f743ebdfddb89b2a21cc808def534
raphaelrpl <raphael.wcosta@gmail.com>
2016-11-07 13:13:49 -0200

    adding ci scripts for doc generation on Unix-Like environment

--- build/scripts/linux/terrame-ci-doc.sh
+++ build/scripts/linux/terrame-ci-doc.sh
@@ -1,31 +1,32 @@
 #!/bin/bash -l
+
 # 
-## It enables to run terrame unittest on UNIX environments.
+## It enables to run terrame doc generation on UNIX environments.
 ## **Remember to export some variables before**
 # 
 ## TERRAME_PATH - Path to TerraME installation
 ## TERRAME_GIT_DIR - Path to TerraME git directory
 #
 ## Usage
-# ./terrame-ci-unittest.sh PACKAGE COMMIT
+# ./terrame-ci-doc.sh PACKAGE COMMIT
 # 
 # PACKAGE - Represents which package will be tested
 # COMMIT - Pull Request Commit Hash used to notify GitHub
 
 # GitHub Status Name
-GITHUB_CONTEXT="Functional test of package $1"
+GITHUB_CONTEXT="Documentation of package $1"
+GITHUB_STATUS="pending"
+GITHUB_DESCRIPTION="Running."
+TARGET_URL="$BUILD_URL/consoleFull"
 # GitHub Commit hash
 COMMIT=$2
-# GitHub Status URL
-TARGET_URL="$BUILD_URL/consoleFull"
+# Sending pending status
+$TERRAME_GIT_DIR/build/scripts/linux/terrame-ci-notify.sh $COMMIT "$GITHUB_CONTEXT" "$GITHUB_STATUS" "$TARGET_URL" "$GITHUB_DESCRIPTION"
 
-# Exporting TerraME environment variables
+# Exporting terrame vars
 export TME_PATH="$TERRAME_PATH/bin"
 export PATH=$PATH:$TME_PATH
 export LD_LIBRARY_PATH=$TME_PATH
-
-# Sending initial status (pending) to GitHub
-$TERRAME_GIT_DIR/build/scripts/linux/terrame-ci-notify.sh $COMMIT "$GITHUB_CONTEXT" "pending" "$TARGET_URL" "Running tests."
 
 terrame -version
 
@@ -34,9 +35,9 @@ TERRAME_COMMANDS=""
 if [ "$1" != "base" ]; then
   TERRAME_COMMANDS="-package $1"
 fi
-
-terrame -color $TERRAME_COMMANDS -test test.lua 2> /dev/null
-# Retrieving TerraME exit code
+# Execute TerraME doc generation
+terrame -color $TERRAME_COMMANDS -doc 2> /dev/null
+# Retrieve TerraME exit code
 RESULT=$?
 
 if [ $RESULT -eq 0 ]; then
@@ -49,8 +50,6 @@ else
   GITHUB_STATUS="failure"
   GITHUB_DESCRIPTION="$RESULT tests failed"
 fi
-# Removing log files
-rm -rf .terrame*
 
 $TERRAME_GIT_DIR/build/scripts/linux/terrame-ci-notify.sh $COMMIT "$GITHUB_CONTEXT" "$GITHUB_STATUS" "$TARGET_URL" "$GITHUB_DESCRIPTION"
 
